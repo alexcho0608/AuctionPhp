78955f46d5287501bf78b623442ce7545a0970f3
alexcho0608 <alexcho0608@users.noreply.github.com>
2015-11-06 20:49:06 +0200

    Update getSearch.php

--- getSearch.php
+++ getSearch.php
@@ -1,16 +1,17 @@
 <?php
-if(isset($_POST["brandp"])&&isset($_POST["modelp"]))
+if(isset($_POST["brand"]) && isset($_POST["model"]))
 {
-	$con=mysqli_connect("localhost","root","","test");
+    $con=mysqli_connect("localhost","root","","test");
     if(mysqli_connect_errno($con))
     { 
         echo "Failed to connect".mysqli_connect_error();
     }
-    $brand=mysqli_real_escape_string($con,$_POST["brandp"]);
-    $model=mysqli_real_escape_string($con,$_POST["modelp"]);
-    $msg="SELECT * FROM carsinfo WHERE Model='$model' AND Car='$brand'";
-    $answ=mysqli_query($con,$msg);
-    $str="<center> <table id='bet' width=100% style='background-color:#008822';>
+    $brand = mysqli_real_escape_string($con,$_POST["brand"]);
+    $model = mysqli_real_escape_string($con,$_POST["model"]);
+    $year = mysqli_real_escape_string($con, $_POST["year"]);
+    $msgSql = "SELECT * FROM carsinfo WHERE Model='$model' AND Car='$brand' AND Year >= '$year'";
+    $answ = mysqli_query($con,$msgSql);
+    $tableStr = "<center> <table id='bet' width=100% style='background-color:#008822';>
         <thead id='res' style='color:orange;text-weight:bold;'>
         <th style='width:6%'>ID</th>
         <th style='width:6%'>Кола</th>
@@ -26,19 +27,19 @@ if(isset($_POST["brandp"])&&isset($_POST["modelp"]))
         </thead>";
      while($row=mysqli_fetch_array($answ))
      {
-     	$str=$str."<tr> 
-     	<td id='id'><center>".$row['Id']."</center></td>
-     	<td><center>" .$row['Car']."</center></td>
-     	<td><center>".$row['DateofProduction']."год.</center></td>
-     	<td><center>".$row['Model']."</center></td>
-     	<td id='price'><center>".$row['BidPrice']."лв.</center></td>
-     	<td><center>".$row['Expire']."</center></td>
-     	<td><center><a href='Image/".$row['image']."'><img height=30 width=30 src='Image/".$row['image']."''></img></a></center></td>
-     	<td ><center>".$row['sellerName']."</center></td>
-     	<td id='buyer'><center>".$row['buyerName']."</center></td>
-     	</td><td><center><input type=text class='myClass' id='".$row['Id']."'/></center></td></tr>";
+     	$tableStr = $tableStr . "<tr> 
+     	<td id='id'><center>" . $row['Id'] . "</center></td>
+     	<td><center>" . $row['Car'] . "</center></td>
+     	<td><center>" . $row['DateofProduction'] . "год.</center></td>
+     	<td><center>" . $row['Model'] . "</center></td>
+     	<td id='price'><center>" . $row['BidPrice'] . "лв.</center></td>
+     	<td><center>" . $row['Expire'] . "</center></td>
+     	<td><center><a href='Image/" . $row['image'] . "'><img height=30 width=30 src='Image/" . $row['image']."''></img></a></center></td>
+     	<td ><center>" . $row['sellerName'] . "</center></td>
+     	<td id='buyer'><center>" . $row['buyerName'] . "</center></td>
+     	</td><td><center><input type=text class='myClass' id='" . $row['Id'] . "'/></center></td></tr>";
      }
-    $str=$str."</table></center>";
-    echo $str;
+    $tableStr=$tableStr . "</table></center>";
+    echo $tableStr;
 }
 ?>

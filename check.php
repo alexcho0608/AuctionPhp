7ef6a9341015410e4e9c8c4927e60535695f4056
alexcho0608 <alexcho0608@users.noreply.github.com>
2015-11-06 20:30:18 +0200

    Update check.php

--- check.php
+++ check.php
@@ -1,11 +1,12 @@
+// this file is for testing purposes,it isn't used in the project
 <?php
-$con=mysqli_connect("localhost","root","","test");
+$con=mysqli_connect("localhost","root","","");
     if(mysqli_connect_errno($con))
     { 
     	echo "Failed to connect".mysqli_connect_error();
     }
-    $ip=mysqli_real_escape_string($_GET['ip']);
-    $answ=mysqli_query($con,"SELECT * FROM banned WHERE ip='$ip'");
+    $ip = mysqli_real_escape_string($_GET['ip']);
+    $answ = mysqli_query($con,"SELECT * FROM banned WHERE ip='$ip'");
     if($answ->num_rows>0) echo 1;
     else echo 0;
 ?>

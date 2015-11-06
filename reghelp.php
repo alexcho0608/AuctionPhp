ac84ca04824e6b6e34580abc83e70c7a6eb6dd09
alexcho0608 <alexcho0608@users.noreply.github.com>
2015-11-06 22:31:30 +0200

    Update reghelp.php

--- reghelp.php
+++ reghelp.php
@@ -6,28 +6,28 @@ function validateMail($str)
         header("Location:register.php");die();
     }
 }
-if(isset($_POST["name"])&&isset($_POST["password"])&&isset($_POST["e-mail"]))
+if(isset($_POST["name"]) && isset($_POST["password"]) && isset($_POST["e-mail"]))
 {
-	validateMail($_POST["e-mail"]);
-	$con=mysqli_connect("localhost","root","","test");
+    validateMail($_POST["e-mail"]);
+    $con=mysqli_connect("localhost","root","","test");
     if(mysqli_connect_errno($con))
     { 
         echo "Failed to connect".mysqli_connect_error();
     }
-    $a=mysqli_real_escape_string($con,$_POST["name"]);
-    $b=mysqli_real_escape_string($con,$_POST["password"]);
-    $c=mysqli_real_escape_string($con,$_POST["e-mail"]);
-    $ip=$_SERVER['REMOTE_ADDR'];
-    $msg="SELECT * FROM users WHERE User_Name='$a' OR e_mail='$c'";
-    $answ=mysqli_query($con,$msg);
-    if($answ->num_rows==0)
+    $username = mysqli_real_escape_string($con,$_POST["name"]);
+    $password = md5(mysqli_real_escape_string($con,$_POST["password"]));
+    $email = mysqli_real_escape_string($con,$_POST["e-mail"]);
+    $ip = $_SERVER['REMOTE_ADDR'];
+    $msgSql = "SELECT * FROM users WHERE User_Name='$username' OR e_mail='$email'";
+    $answer = mysqli_query($con,$msgSql);
+    if($answer->num_rows == 0)
     {
-    	$msg="INSERT INTO  users (User_Name,Password,e_mail,ip) VALUES ('$a','$b','$c','$ip')";
-    	$bool=true;
-        $bool=mysqli_query($con,$msg);
+    	$msgInsertSql = "INSERT INTO  users (User_Name,Password,e_mail,ip) VALUES ('$username','$password','$email','$ip')";
+    	$bool = true;
+        $bool = mysqli_query($con,$msgInsertSql);
         //var_dump($bool);
-    	setcookie("name",$a,time()+24*60*60);
-    	setcookie("password",md5($b),time()+24*60*60);
+    	setcookie("name",$username,time()+24*60*60);
+    	setcookie("password",md5($password),time()+24*60*60);
     	header("Location:index.php");
     }
     else

7e8a86c33a38f04f0d4ca9a730bb46b046266aa8
alexcho0608 <alexcho0608@users.noreply.github.com>
2015-11-06 22:34:01 +0200

    Update reghelp.php

--- reghelp.php
+++ reghelp.php
@@ -6,7 +6,7 @@ function validateMail($str)
         header("Location:register.php");die();
     }
 }
-if(isset($_POST["name"]) && isset($_POST["password"]) && isset($_POST["e-mail"]))
+if(isset($_POST["username"]) && isset($_POST["password"]) && isset($_POST["email"]))
 {
     validateMail($_POST["e-mail"]);
     $con=mysqli_connect("localhost","root","","test");
@@ -14,9 +14,9 @@ if(isset($_POST["name"]) && isset($_POST["password"]) && isset($_POST["e-mail"])
     { 
         echo "Failed to connect".mysqli_connect_error();
     }
-    $username = mysqli_real_escape_string($con,$_POST["name"]);
+    $username = mysqli_real_escape_string($con,$_POST["username"]);
     $password = md5(mysqli_real_escape_string($con,$_POST["password"]));
-    $email = mysqli_real_escape_string($con,$_POST["e-mail"]);
+    $email = mysqli_real_escape_string($con,$_POST["email"]);
     $ip = $_SERVER['REMOTE_ADDR'];
     $msgSql = "SELECT * FROM users WHERE User_Name='$username' OR e_mail='$email'";
     $answer = mysqli_query($con,$msgSql);
